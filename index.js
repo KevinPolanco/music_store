d706bb18ea1e8fea238b21b45a25ddcd1f55f708
KevinPolanco <polancockp@gmail.com>
2022-04-13 15:31:58 -0400

    Configuracion bootstrap y HBS / Prototipo Navbar y Footer

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const { create } = require("express-handlebars");
 const app = express();
-const getConection = require('./controller/user.controller')
 
-getConection();
+const hbs = create({
+  partialsDir: ["views/components"],
+  extname: ".hbs"
+})
+
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+app.use(express.static("node_modules/bootstrap/dist"))
+
+app.use('/', require('./routes/vistas.router'))
 
 app.listen(3000, () => {console.log(`Server ON, in port 3000`)})
