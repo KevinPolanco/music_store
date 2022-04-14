6aa000dac216b229b45599e9055a68cb72fc4155
KevinPolanco <polancockp@gmail.com>
2022-04-14 17:17:43 -0400

    Creacion de vistas store, collection y adminAlbums

--- routes/vistas.router.js
+++ routes/vistas.router.js
@@ -5,5 +5,8 @@ router.get("/", (req, res) => {res.render('home')})
 router.get("/login", (req, res) => {res.render('login')})
 router.get("/register", (req, res) => {res.render('register')})
 router.get("/createalbum", (req, res) => {res.render('createalbum')})
+router.get("/store", (req, res) => {res.render('store')})
+router.get("/myCollection", (req, res) => {res.render('mycollection')})
+router.get("/adminAlbums", (req, res) => {res.render('adminalbums')})
 
 module.exports = router;
