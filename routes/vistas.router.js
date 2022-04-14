e09b5c4b0c5f8c489ff41f54592c199f74d322ff
KevinPolanco <polancockp@gmail.com>
2022-04-14 12:27:49 -0400

    Creacion vistas login, registro usuario y registro album

--- routes/vistas.router.js
+++ routes/vistas.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {res.render('home')})
+router.get("/login", (req, res) => {res.render('login')})
 router.get("/register", (req, res) => {res.render('register')})
+router.get("/createalbum", (req, res) => {res.render('createalbum')})
 
 module.exports = router;
