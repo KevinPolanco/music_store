ce7c5e2703b75140b60d5f2bc567c48463cf3fdd
KevinPolanco <polancockp@gmail.com>
2022-05-02 17:10:01 -0400

    se agrego links a artist y label en vistas myCollection y store

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+
+const getLinkLabel = (labelName) => {
+  if (labelName === "Blue Note") return "https://www.bluenote.com/";
+  if (labelName === "Columbia") return "http://www.columbiarecords.com/";
+  if (labelName === "Impulse!") return "http://www.impulserecords.com/";
+}
+
